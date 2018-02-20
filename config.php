575da47bbbc7dd5f39449a8c0f9572a1d9953ebc
alfianasmar <alfianasmar@gmail.com>
2018-02-20 09:09:53 +0800

    config

--- config.php
+++ config.php
@@ -2,18 +2,15 @@
 	define('DS', DIRECTORY_SEPARATOR);
 	$httpHost=$_SERVER['HTTP_HOST'];
 	if (strpos($httpHost,'localhost')!==false) {
-		define('BASE_URL','http://'.$httpHost.DS.'batubata'.DS);
+		define('BASE_URL','http://'.$httpHost.DS.'ordespembelian'.DS);
 	}else{
 		define('BASE_URL','http://'.$httpHost.DS);
 	}
 	define('BASE_PATH', __DIR__ . DS);
-	define('VIEW_PATH', BASE_PATH.'view' . DS);
-	define('ASSET_PATH', BASE_URL.'asset'.DS);
-	define('UPLOAD_PATH',BASE_PATH.'upload'.DS);
-	define('UPLOAD_URL',BASE_URL.'upload'.DS);
+	define('ASSET_URL', BASE_URL.'assets'.DS);
 
 	define('DB_HOST', 'localhost');
-	define('DB_NAME', 'bb');
+	define('DB_NAME', 'op');
 	define('DB_USER', 'root');
 	define('DB_PASS', '');
 ?>
